48ef376e2eead81e4562ae02beb1cb40c336297b
Yu-Ching Chiang <YUCHI@Yujin.local>
2020-07-31 11:11:21 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+(function ($) {
+  $(document).ready(function (){
+    
+    $('.main-carousel').flickity({
+      cellAlign: 'left',
+      contain: true,
+      wrapAround: 'true',
+      draggable: false,
+      autoPlay: false,
+    });    
+          
+  });
+
+})(jQuery)
